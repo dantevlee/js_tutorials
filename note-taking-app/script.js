3e9b3398b2e0f24bef404970b1ff08a603f25b01
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2023-11-26 21:13:29 -0600

    tutorial updates

--- note-taking-app/script.js
+++ note-taking-app/script.js
@@ -1,5 +1,64 @@
-const addButton = document.getElementById('add');
+const button = document.getElementById('button');
+const app = document.getElementById('app');
 
-addButton.addEventListener('click', () => {
-  console.log('added')
-})
+function saveNote(note) {
+  localStorage.setItem("note-app", JSON.stringify(note));
+}
+
+function getNotes(){
+  return JSON.parse(localStorage.getItem("note-app") || "[]");
+}
+
+getNotes().forEach((note) => {
+  const noteEl = createNoteEl(note.id, note.content);
+  app.insertBefore(noteEl, button);
+});
+
+function addNote() {
+  const notes = getNotes();
+  const Note = {
+    id: Math.floor(Math.random() * 100000),
+    content: "",
+  }
+  const noteElement = createNoteEl(Note.id, Note.content);
+  app.insertBefore(noteElement, button);
+  notes.push(Note);
+  saveNote(notes);
+}
+
+function createNoteEl(id, content) {
+  const noteText = document.createElement('textarea');
+  noteText.classList.add('notes');
+  noteText.placeholder = "Enter a note..";
+  noteText.value = content;
+
+  noteText.addEventListener("dblclick", () => {
+    const warning = confirm("Do you want to delete this note?");
+    if (warning) {
+      deleteNote(id, noteText);
+    }
+  });
+
+  noteText.addEventListener("input", () => {
+    updateNote(id, noteText.value);
+  });
+
+  return noteText;
+}
+
+function deleteNote(id, noteEl) {
+  const notes = getNotes().filter((note) => note.id != id);
+  saveNote(notes);
+  app.removeChild(noteEl);
+}
+
+
+function updateNote(id, content) {
+  const notes = getNotes();
+  const target = notes.filter((note) => note.id == id)[0];
+  target.content = content;
+  saveNote(notes);
+}
+
+
+button.addEventListener('click', addNote);
